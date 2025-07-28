aef72049844183e60796827f1f4545ea403a0626
donmor <donmor3000@hotmail.com>
2025-07-29 07:58:57 +0800

    bump to v7.2.0

--- .github/workflows/.extras.sh
+++ .github/workflows/.extras.sh
@@ -3,9 +3,9 @@
 # Get go from apt
 #sudo apt-get build-dep .
 
-# Get go 1.23 from Google
+# Get go 1.24 from Google
 sudo apt-get install build-essential debhelper
-curl -o /tmp/go.tar.gz https://dl.google.com/go/go1.23.11.linux-`dpkg --print-architecture`.tar.gz
+curl -o /tmp/go.tar.gz https://dl.google.com/go/go1.24.5.linux-`dpkg --print-architecture`.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local/lib -xzf /tmp/go.tar.gz
 sudo ln -sf ../lib/go/bin/go /usr/local/bin/go

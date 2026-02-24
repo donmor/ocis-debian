53b7d3e34b2a6fbed9a4a5700ccc329ca711a0b4
donmor <donmor3000@hotmail.com>
2026-02-24 08:18:14 +0800

    bump to v8.0.0

--- .github/workflows/.extras.sh
+++ .github/workflows/.extras.sh
@@ -3,10 +3,10 @@
 # Get go from apt
 #sudo apt-get build-dep .
 
-# Get go 1.24 from Google
+# Get go 1.25 from Google
 sudo apt-get install build-essential debhelper
-curl -o /tmp/go.tar.gz https://dl.google.com/go/go1.24.5.linux-`dpkg --print-architecture`.tar.gz
-sudo rm -rf /usr/local/go
+curl -o /tmp/go.tar.gz https://dl.google.com/go/go1.25.7.linux-`dpkg --print-architecture`.tar.gz
+sudo rm -rf /usr/local/lib/go
 sudo tar -C /usr/local/lib -xzf /tmp/go.tar.gz
 sudo ln -sf ../lib/go/bin/go /usr/local/bin/go
 sudo ln -sf ../lib/go/bin/gofmt /usr/local/bin/gofmt

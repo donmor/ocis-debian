69c5586f4bbbbdfb271521aa73be54850e8a0e60
donmor <donmor3000@hotmail.com>
2026-05-09 08:16:57 +0800

    bump to v8.0.2, fix go toolchain fetching script

--- .github/workflows/.extras.sh
+++ .github/workflows/.extras.sh
@@ -3,12 +3,18 @@
 # Get go from apt
 #sudo apt-get build-dep .
 
-# Get go 1.25 from Google
+# Get go from Google
+go_req=`grep '^go [0-9.]\+$' go.mod | grep -o '[0-9.]\+'`
+go_arch=`dpkg --print-architecture`
+go_pkg=`curl -sL "https://go.dev/dl/?mode=json&include=all" | jq -r --arg major ${go_req} --arg arch ${go_arch}  'first(.[] | select(.version | contains($major))) | .files[] | select(.os == "linux" and .arch == $arch) | .filename'`
+echo Required go version: ${go_req}
 sudo apt-get install build-essential debhelper
-curl -o /tmp/go.tar.gz https://dl.google.com/go/go1.25.7.linux-`dpkg --print-architecture`.tar.gz
-sudo rm -rf /usr/local/lib/go
+sudo apt remove golang-*-go
+echo Fetching ${go_pkg}...
+curl -L -o /tmp/go.tar.gz https://dl.google.com/go/${go_pkg}
+sudo rm -rf /usr/local/go /usr/local/lib/go
 sudo tar -C /usr/local/lib -xzf /tmp/go.tar.gz
 sudo ln -sf ../lib/go/bin/go /usr/local/bin/go
 sudo ln -sf ../lib/go/bin/gofmt /usr/local/bin/gofmt
-sudo apt remove golang-*-go
+echo Using Go: `which go`
 # End hack
